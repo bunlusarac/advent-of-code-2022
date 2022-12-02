331db9878dbd95f8b10dbe29ce0135934649581a
Barış Ünlüsaraç <7127523+bunlusarac@users.noreply.github.com>
2022-12-02 19:15:00 +0300

    day1 solution

---
+++ day1/calorie_counting.py
@@ -0,0 +1,20 @@
+from functools import reduce
+txt = open("input.txt","r")
+
+def reducer(acc, cal):
+    if cal.strip():
+        return acc[:-1] + [[acc[-1][0] + [cal], acc[-1][1] + int(cal)]]
+    else:
+        return acc + [[[], 0]]
+
+elf_vector = reduce(reducer, txt, [[[], 0]])
+txt.close()
+
+elf_vector.sort(reverse=True, key=lambda t: t[1])
+print(elf_vector[0][1])
+print(reduce(lambda acc, x: acc + x[1], elf_vector[:3], 0))
+
+
+
+
+
